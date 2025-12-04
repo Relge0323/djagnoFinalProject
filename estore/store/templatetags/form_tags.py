0fdc4edac4a284be11108ebb6a93e04e86b0e139
Relge0323 <megler@hotmail.com>
2025-12-04 11:03:01 -0600

    added comment to form_tags.py

--- estore/store/templatetags/form_tags.py
+++ estore/store/templatetags/form_tags.py
@@ -1,7 +1,17 @@
 from django import template
 
+#create a new template library instance so we can register custom filters
 register = template.Library()
 
 @register.filter(name='add_class')
 def add_class(field, css):
+    """
+    Custom template filter to add a CSS class to a Django form field
+    
+    used with username field in the login.html and signup.html files
+      {{form.username|add_class:"form-control"}}
+
+    this renders the input with the specified class
+        <input type="text" name="username" class="form-control">
+    """
     return field.as_widget(attrs={"class": css})
